2924f3c506a61d64ce31d4c8204b1dc23a3e573a
Suman Dhara <sumandhara341@gmail.com>
2025-07-17 16:16:31 +0530

    build issue resolve

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import store from "./store/Store.ts";
 import App from "./App.tsx";
 import { AwsRum } from "aws-rum-web";
 
+// Declare them separately
+const APPLICATION_ID = "181c4d9c-839e-4e8b-9e44-3085430f298b";
+const APPLICATION_VERSION = "1.0.0";
+const APPLICATION_REGION = "us-east-1";
+
+// Config object (must NOT include applicationId etc.)
 const config = {
   sessionSampleRate: 1,
   identityPoolId: "us-east-1:fa34c40d-fe0a-40dc-bfc8-e041cfce03a8",
@@ -13,18 +19,10 @@ const config = {
   allowCookies: true,
   enableXRay: true,
   signing: true,
-  APPLICATION_ID: "181c4d9c-839e-4e8b-9e44-3085430f298b",
-  APPLICATION_VERSION: "1.0.0",
-  APPLICATION_REGION: "us-east-1",
 };
 
 try {
-  new AwsRum(
-    config.APPLICATION_ID,
-    config.APPLICATION_REGION,
-    config.identityPoolId,
-    config
-  );
+  new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, config);
 } catch (error) {
   console.warn("Error initializing RUM:", error);
 }
